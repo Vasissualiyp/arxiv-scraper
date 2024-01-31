f2a36d47bf24662d257bd3857a41a130569fc699
Vasissualiyp <vasily.pustovoit@mail.utoronto.ca>
2024-01-30 23:13:56 -0500

    The compilation of the document now bypasses errors without problems

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#OUTPUT_FOLDER="$HOME/research/notes/papers/arxiv_scraper/"
-OUTPUT_FOLDER="./output/"
+OUTPUT_FOLDER="$HOME/research/notes/papers/arxiv_scraper/"
+#OUTPUT_FOLDER="./output/"
 
 # Get the directory where the script is located 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
@@ -16,7 +16,7 @@ mv ./workdir/related_papers.tex "$OUTPUT_FOLDER"
 
 # Compile the document
 cd "$OUTPUT_FOLDER" || exit 1
-pdflatex "related_papers.tex"
+pdflatex -interaction=nonstopmode "related_papers.tex"
 # Cleanup the folder - only leave .tex and .pdf files
 for file in *; do
     if [[ ! $file =~ \.tex$ ]] && [[ ! $file =~ \.pdf$ ]]; then
