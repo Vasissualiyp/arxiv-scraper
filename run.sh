0318199f82d5dc2ee25cad9f89901199bc82ef11
Vasissualiyp <vasily.pustovoit@mail.utoronto.ca>
2024-02-01 13:46:51 -0500

    The run.sh script now is taking the files from the config.ini

--- run.sh
+++ run.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
-OUTPUT_FOLDER="$HOME/research/notes/papers/arxiv_scraper/"
-#OUTPUT_FOLDER="./output/"
-
-# Get the directory where the script is located 
+# Get the directory where the script is located
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-# cd into that directory, if haven't done that already
+
+# Path to the config file
+CONFIG_FILE="$SCRIPT_DIR/config/config.ini"
+
+# Read parameters from the config.ini file
+OUTPUT_FOLDER=$(awk -F " = " '/OutputFinalFolder/ {print $2}' "$CONFIG_FILE")
+OUTPUT_TEX_FILE=$(awk -F " = " '/OutputTeXFile/ {print $2}' "$CONFIG_FILE")
+OUTPUT_SPEECH_FILE=$(awk -F " = " '/OutputSpeechFile/ {print $2}' "$CONFIG_FILE")
+
+# Ensure OUTPUT_FOLDER is evaluated for variables such as $HOME
+eval OUTPUT_FOLDER=$OUTPUT_FOLDER
+
+# cd into the script directory, if haven't done that already
 cd "$SCRIPT_DIR"
 
 # Run the script
 source ./env/bin/activate
 python ./src/main.py
 
-mv ./workdir/related_papers.tex "$OUTPUT_FOLDER"
-mv ./workdir/tts_papers.mp3 "$OUTPUT_FOLDER"
+# Move the files to the output folder
+mv "./workdir/$OUTPUT_TEX_FILE" "$OUTPUT_FOLDER"
+mv "./workdir/$OUTPUT_SPEECH_FILE" "$OUTPUT_FOLDER"
 
 # Compile the document
-cd "$OUTPUT_FOLDER" || exit 1
-pdflatex -interaction=nonstopmode "related_papers.tex"
-# Cleanup the folder - only leave .tex and .pdf files
+cd "$OUTPUT_FOLDER" || { echo "Output folder doesn't exist! Exiting..."; exit 1; }
+pdflatex -interaction=nonstopmode "$OUTPUT_TEX_FILE"
+
+# Cleanup the folder - only leave .tex, .pdf, and .mp3 files
 for file in *; do
     if [[ ! $file =~ \.tex$ ]] && [[ ! $file =~ \.pdf$ ]] && [[ ! $file =~ \.mp3$ ]]; then
         rm "$file"
     fi
 done
-
 
