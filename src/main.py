d012294ec7612ec58e1bc9540ed6dc8226e4c210
Vasissualiyp <vasily.pustovoit@mail.utoronto.ca>
2024-02-11 12:56:39 -0500

    Refactored the main function

--- src/main.py
+++ src/main.py
@@ -12,51 +12,42 @@ from doc_to_speech import tts_with_video_main
 from create_movie import create_video_files
 from create_movie import combine_videos
 
-def create_movie_main():
+def main(date = "current", combined_file_flag = False):
+    """
+    The main 
+    """
     # Extract config
     config = extract_configuration('config/config.ini')
 
-    date = "2024-02-07"
+    if date == "current":
+        # Scrape arxiv for recent papers
+        papers = scrape_arxiv_new_submissions(config.ArxivURL)
+        #archived_list_filename = 'workdir/archive.csv'
+        #write_tuples_to_csv(papers, config)
+        
+        # Categorize recent papers with ChatGPT
+        related_papers = ai_categorization_main(papers, config)
+        # The function above will also add the discovered arxiv pages to the csv file
 
-    related_papers = get_arxiv_numbers_for_date(config, date)
+    else:
+        related_papers = get_arxiv_numbers_for_date(config, date)
 
     # Now, put the papers to the json file, which will be later used to create the tex file
     dump_papers_to_json(related_papers, config)
     
-    # Create a tex files with all relevant papers
-    create_all_speech_files_main(config)
-    
-    # Create speech and tex files
-    tts_with_video_main(config)
-    
-    # Create separate video files
-    #create_video_files(config)
-    #combine_videos(config)
-    
-
-def main():
-    # Extract config
-    config = extract_configuration('config/config.ini')
-    
-    # Scrape arxiv for recent papers
-    papers = scrape_arxiv_new_submissions(config.ArxivURL)
-    #archived_list_filename = 'workdir/archive.csv'
-    #write_tuples_to_csv(papers, config)
-    
-    # Categorize recent papers with ChatGPT
-    related_papers = ai_categorization_main(papers, config)
-    # The function above will also add the discovered arxiv pages to the csv file
-
-    # Now, put the papers to the json file, which will be later used to create the tex file
-    dump_papers_to_json(related_papers, config)
-    
-    # Create a tex file with all relevant papers
-    create_tex_main(config)
-    
-    # Create text-to-speech summary of the papers
-    tts_main(config)
+    if combined_file_flag: 
+        # Create a tex file with all relevant papers
+        create_tex_main(config)
+        
+        # Create text-to-speech summary of the papers
+        tts_main(config)
+    else:
+        # Create a tex files with all relevant papers
+        create_all_speech_files_main(config)
+        
+        # Create speech and tex files
+        tts_with_video_main(config)
 
 if __name__ == '__main__':
-    #main()
-    create_movie_main()
+    main(date = 'current', combined_file_flag = False)
 
