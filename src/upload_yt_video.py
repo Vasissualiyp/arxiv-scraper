1561f60bf2924f34ce4eae276df1b581ad417b9e
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2024-02-11 20:32:16 -0500

    Added the option to load credentials from the pickle file

--- src/upload_yt_video.py
+++ src/upload_yt_video.py
@@ -8,11 +8,44 @@ from googleapiclient.http import MediaFileUpload
 SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
 CLIENT_SECRETS_FILE = './config/credentials.json'
 
+import os
+import pickle
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+# Set up OAuth 2.0
+SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
+CLIENT_SECRETS_FILE = './config/credentials.json'
+CREDENTIALS_PICKLE_FILE = './config/token.pickle'  # Path to save credentials
+
+def save_credentials(credentials):
+    with open(CREDENTIALS_PICKLE_FILE, 'wb') as token:
+        pickle.dump(credentials, token)
+
 def get_authenticated_service():
-    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-    # Use run_local_server instead of run_console
-    credentials = flow.run_local_server(port=0)
+    credentials = None
+    # Check if credentials pickle file exists
+    if os.path.exists(CREDENTIALS_PICKLE_FILE):
+        print("Loading Credentials From File...")
+        with open(CREDENTIALS_PICKLE_FILE, 'rb') as token:
+            credentials = pickle.load(token)
+    # If there are no valid credentials available, let the user log in.
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            print("Refreshing Access Token...")
+            credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+            credentials = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        save_credentials(credentials)
     return build('youtube', 'v3', credentials=credentials)
+
+#def get_authenticated_service():
+#    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+#    # Use run_local_server instead of run_console
+#    credentials = flow.run_local_server(port=0)
+#    return build('youtube', 'v3', credentials=credentials)
 
 
 def initialize_upload(youtube, file):
@@ -37,7 +70,6 @@ def initialize_upload(youtube, file):
         body=body,
         media_body=media
     ).execute()
-
     print(f'Video uploaded. Video ID: {response_upload["id"]}')
 
 if __name__ == '__main__':
